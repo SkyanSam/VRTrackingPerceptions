b184705a0e241286611be3673a121042ec1c57f6
SkyanSam <samuelwiggins15@gmail.com>
2023-10-30 17:48:26 -0400

    attempt to add packages

---
+++ Assets/CharacterAIBehaviour.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CharacterAI;
+using UnityEngine.Windows.Speech;
+using System.Linq;
+using System.Net;
+
+public class CharacterAIBehaviour : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
