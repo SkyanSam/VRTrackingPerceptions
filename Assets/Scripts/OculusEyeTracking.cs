82b3bf98c0f86e2e2f539d81051cb2bad61e71a6
SkyanSam <samuelwiggins15@gmail.com>
2023-11-08 19:33:19 -0500

    eye tracking setup for meta quest pro

---
+++ Assets/Scripts/OculusEyeTracking.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.XR.Oculus;
+public class OculusEyeTracking : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        OculusEyeTracking.
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
