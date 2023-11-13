b724637653b583a2d859e736ed00a5f04bf6931a
SkyanSam <31390605+SkyanSam@users.noreply.github.com>
2023-11-13 18:32:55 -0500

    oculus sdk eyetracking setup!

--- Assets/Scripts/OculusEyeTracking.cs
+++ Assets/Scripts/OculusEyeTracking.cs
@@ -7,7 +7,7 @@ public class OculusEyeTracking : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        OculusEyeTracking.
+        //OculusEyeTracking.
     }
 
     // Update is called once per frame
