bcd210f6de5c5bb685f1c00f7cd51122d34011a4
SkyanSam <31390605+SkyanSam@users.noreply.github.com>
2023-11-14 20:11:10 -0500

    THE EYE TRACKING FOR META QUEST PRO WORKS :D

--- Assets/Scripts/OculusEyeTracking.cs
+++ Assets/Scripts/OculusEyeTracking.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 using Unity.XR.Oculus;
 public class OculusEyeTracking : MonoBehaviour
 {
+    public Camera cam;
+    public GameObject leftEye;
+    public GameObject rightEye;
+    public float raycastMaxDistance = 500;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,21 @@ public class OculusEyeTracking : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        var gaze = CalculateGaze();
+        RaycastHit hit;
+        var isHit = Physics.Raycast(new Ray(cam.transform.position, cam.transform.rotation * gaze), out hit, raycastMaxDistance, LayerMask.GetMask("EyeTracking"));
+        //Gizmos.DrawLine(cam.transform.position, cam.transform.rotation * gaze);
+        if (isHit)
+        {
+            print("HIT SUCCESSFUL");
+            var color = hit.collider.GetComponent<MeshRenderer>().material.color;
+            color = Vector4.MoveTowards(color, Color.red, Time.deltaTime);
+            hit.collider.GetComponent<MeshRenderer>().material.color = color;
+        }
+    }
+    // Calculating the Gaze by getting mean of left and right eye rotation
+    Vector3 CalculateGaze()
+    {
+        return ((rightEye.transform.rotation * Vector3.forward) + (leftEye.transform.rotation * Vector3.forward)) / 2;
     }
 }
